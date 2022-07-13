071182fb802ff49fc8570dd0a8c7c1137820803d
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-07-13 21:48:20 +0530

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App Component Test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Hello World');
   expect(linkElement).toBeInTheDocument();
 });
