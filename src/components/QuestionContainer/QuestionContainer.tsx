6fcc59e5d6b8400de63ff3207b21352c202d1d8e
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-08-04 18:44:51 +0530

    Add question container

---
+++ src/components/QuestionContainer/QuestionContainer.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+const QuestionContainer = () => {
+    return (
+      <>
+        <Container maxWidth='md'>
+            
+        </Container>
+      </>
+    );
+}
+
+export { QuestionContainer };
