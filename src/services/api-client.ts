bb90bb9a026e020f89c2439a23b2d6197f98523e
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-08-04 18:45:01 +0530

    Create api-client.ts

---
+++ src/services/api-client.ts
@@ -0,0 +1,22 @@
+export const apiClient = {
+  getCategories: async () => {
+    const responce = await fetch('https://the-trivia-api.com/api/categories');
+    const result = await responce.json();
+    return result;
+  },
+  getQuetstions: async () => {
+    const responce = await fetch('https://the-trivia-api.com/api/questions?limit=5');
+    const result = await responce.json();
+    return result;
+  },
+  getLevel: async () => {
+    const responce = await fetch('https://the-trivia-api.com/api/categories');
+    const result = await responce.json();
+    return result;
+  },
+  getLevelTest: async () => {
+    const responce = await fetch('https://the-trivia-api.com/api/categories');
+    const result = await responce.json();
+    return result;
+  }
+};
