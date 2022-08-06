ca600df72b502bf7ab19e70f18e459b54748e301
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-08-06 19:10:44 +0530

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 export const apiClient = {
   getCategories: async () => {
-    const responce = await fetch('https://the-trivia-api.com/api/categories');
-    const result = await responce.json();
+    const responce = await axios.get('https://the-trivia-api.com/api/categories');
+    const result = await responce.data;
     return result;
   },
   getQuetstions: async () => {
