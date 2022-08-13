8e338bf92dd59127f523be7ac7c0af4f6e6d6ccf
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-08-13 22:48:54 +0530

    Update Question.tsx

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,8 +1,10 @@
-import { Box } from "@mui/material";
+import { Box } from '@mui/material';
 
 const Question = () => {
+  <>
     <Box></Box>
-     <Box></Box>
-}; 
+    <Box></Box>
+  </>;
+};
 
 export { Question };
